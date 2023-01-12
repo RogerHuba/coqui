c4237a87e3e146a8e09010ee1606baee78153b3d
Roger Huba <rogerhuba@gmail.com>
2023-01-12 01:04:21 -0600

    fix css issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      './templates/**/*.html',
-      './node_modules/flowbite/**/*.js'
-  ],
+  content: ['./templates/**/*.html',
+        './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [
-      require('flowbite/plugin')
-  ],
+  plugins: [require('flowbite/plugin')],
 }
